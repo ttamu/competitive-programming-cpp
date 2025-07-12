fa4aa43284916169860cf64daa80621efe232c5f
tamura <tomoki0927.7939@icloud.com>
2025-07-12 20:09:54 +0900

    feat(arc198): add solution for B

--- src/atcoder/arc198/b.cpp
+++ src/atcoder/arc198/b.cpp
@@ -2,4 +2,24 @@
 using namespace std;
 using ll = long long;
 
-int main() {}
+bool ok(ll X, ll Y, ll Z) {
+    if (Y % 2 == 0) {
+        if (X < Z) return false;
+        if (Y > 2 * X) return false;
+    } else {
+        if (Z < 1) return false;
+        if (X < Z) return false;
+        if (Y + 1 > 2 * X) return false;
+    }
+    return true;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        ll X, Y, Z;
+        cin >> X >> Y >> Z;
+        cout << (ok(X, Y, Z) ? "Yes" : "No") << endl;
+    }
+}
