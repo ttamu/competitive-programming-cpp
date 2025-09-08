5bff4d6dab0a9ab8da3c47a9b8c162c297e48cec
tamura <tomoki0927.7939@icloud.com>
2025-09-09 01:13:05 +0900

    feat(template): add macros and type aliases

--- src/template.cpp
+++ src/template.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 #define debug(x) cerr << #x << ": " << x << endl
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
 using namespace std;
 using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
 
 int main() {}
